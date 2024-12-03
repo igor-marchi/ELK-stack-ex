9df1d7109811ff9a501cfd95a7ee151fbf2a46c7
Igor Marchi <igoraugustomarchi@gmail.com>
2024-12-03 17:50:51 -0300

    feat: tipar e criar .env

--- app/src/routes/customers.ts
+++ app/src/routes/customers.ts
@@ -1,37 +1,47 @@
 import { Router } from "express";
 import { Client } from "@elastic/elasticsearch";
+import { QueryDslQueryContainer } from "@elastic/elasticsearch/lib/api/types";
+
+interface Document {
+  id: string;
+  email: string;
+  cpf_cnpj: string;
+  name: string;
+  address: string;
+}
 
 const router = Router();
-const client = new Client({ node: "http://localhost:9200" });
+const client = new Client({ node: process.env.ELASTICSEARCH_NODE });
 
 router.get("/customers", async (req, res) => {
   try {
     const { id, email, cpf_cnpj, name } = req.query;
 
-    const query: any = {
+    const mustQueries: QueryDslQueryContainer[] = [];
+
+    if (id) mustQueries.push({ match: { id: id as string } });
+    if (email) mustQueries.push({ match: { email: email as string } });
+    if (cpf_cnpj) mustQueries.push({ match: { cpf_cnpj: cpf_cnpj as string } });
+    if (name) mustQueries.push({ match: { name: name as string } });
+
+    const query: QueryDslQueryContainer = {
       bool: {
-        must: [],
+        must: mustQueries,
       },
     };
 
-    if (id) query.bool.must.push({ match: { id } });
-    if (email) query.bool.must.push({ match: { email } });
-    if (cpf_cnpj) query.bool.must.push({ match: { cpf_cnpj } });
-    if (name) query.bool.must.push({ match: { name } });
-
-    const result = await client.search({
+    const result = await client.search<Document>({
       index: "customer",
-      body: {
-        query,
-      },
+      query,
     });
 
     res.json(
-      result.hits.hits.map((hit: any) => ({
+      result.hits.hits.map((hit) => ({
         id: hit._source?.id,
         email: hit._source?.email,
         cpf_cnpj: hit._source?.cpf_cnpj,
         name: hit._source?.name,
+        address: hit._source?.address,
       }))
     );
   } catch (error) {
